453739388e88c8251e59e7271d6d9afe93272277
bkromrey <146262682+bkromrey@users.noreply.github.com>
2026-01-11 16:39:27 -0600

    formatted main.c

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include "pico/stdlib.h"
 
-int main(){
+int main() {
 
   const uint BOARD_LED = PICO_DEFAULT_LED_PIN;
   gpio_init(BOARD_LED);
   gpio_set_dir(BOARD_LED, GPIO_OUT);
 
-  while(true){
+  while (true) {
     gpio_put(BOARD_LED, 1);
     sleep_ms(500);
     gpio_put(BOARD_LED, 0);
