cf7c73d68d9348af7ee2cf32c5205d9be9d8452e
bkromrey <146262682+bkromrey@users.noreply.github.com>
2026-01-12 17:56:59 -0600

    add github actions to lint & build a placeholder blink program

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include "pico/stdlib.h"
+
+int main() {
+
+  const uint BOARD_LED = PICO_DEFAULT_LED_PIN;
+  gpio_init(BOARD_LED);
+  gpio_set_dir(BOARD_LED, GPIO_OUT);
+
+  while (true) {
+    gpio_put(BOARD_LED, 1);
+    sleep_ms(500);
+    gpio_put(BOARD_LED, 0);
+    sleep_ms(2000);
+  }
+
+  return 0;
+}
