586d2e36a71f5b31f3224a5d154f2e06ae97165b
johndavidlopez14 <johnd.lopez14@gmail.com>
2026-01-18 18:57:27 -0800

    changed pins defines, added pico_enable_stdio_usb in Cmake

--- src/main.c
+++ src/main.c
@@ -5,8 +5,10 @@
 // User Modules
 #include "hardware/led_array.h"
 
-// Reset
-#define RESET_PIN 2
+// Buttons
+#define BUTTON_1 24
+#define BUTTON_2 22
+#define BUTTON_3 16
 
 // LED Array
 #define LED_PINS_LENGTH 6
@@ -19,37 +21,29 @@
 uint Led_Pins[LED_PINS_LENGTH] = {LED_PIN_0, LED_PIN_1, LED_PIN_2, LED_PIN_3, LED_PIN_4, LED_PIN_5};
 
 // LCD I2C
-#define LCD_I2C_SDA 26
-#define LCD_I2C_SCL 27
+#define LCD_I2C_SDA 0
+#define LCD_I2C_SCL 2
 
 // Sensor I2C
-#define SENSOR_I2C_SDA 20
-#define SENSOR_I2C_SCL 21
+#define SENSOR_I2C_SDA 4
+#define SENSOR_I2C_SCL 5
 
-void Reset_Init(uint pin_number){
-  gpio_init(pin_number);
-  gpio_set_dir(pin_number, GPIO_IN);
-  gpio_pull_up(pin_number);
-}
+// ADC Pin
+#define PHOTORESISTOR_ADC 26
+
+// Output Flag
+volatile uint Output_Flag = 0;
+const uint Output_Screens = 2;
 
 int main() {
   // Needed for reboot
   stdio_init_all();
-
-  // Reset
-  Reset_Init(RESET_PIN);
 
   // LED Array
   LED_Array_Init(Led_Pins, LED_PINS_LENGTH);
 
   uint led_value = 0;
   while (true) {
-      if (!gpio_get(RESET_PIN))
-    {
-      reset_usb_boot(0,0);      
-    }
-
-    sleep_ms(500);
     Display_LED_Array(led_value++);
     if(led_value > LED_PINS_LENGTH)
       led_value = 0;
