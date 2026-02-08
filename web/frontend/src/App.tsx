6f63af6be69c977313478e947e2a40680e4ffb07
Eugenia Uvarov <grungefortuna@gmail.com>
2026-02-08 18:16:42 -0500

    test ci frontend

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -3,7 +3,7 @@ export default function App() {
     <div className="min-h-screen bg-slate-900 text-slate-100 flex items-center justify-center">
       <div className="rounded-2xl bg-slate-800 p-8 shadow">
         <h1 className="text-3xl font-bold">Humidity Sensor UI</h1>
-        <p className="mt-2 text-slate-300">Tailwind v4 is working ✅</p>
+        <p className="mt-2 text-slate-300">Tailwind v4 is working !!!!!!</p>
         <button className="mt-6 rounded-xl bg-indigo-500 px-4 py-2 font-medium hover:bg-indigo-400 text-blue-600">
           Test Button
         </button>
