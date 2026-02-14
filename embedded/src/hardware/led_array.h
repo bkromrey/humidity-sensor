0b41f1ed272a3017e586d50c635ab87512c5d99f
johndavidlopez14 <johnd.lopez14@gmail.com>
2026-02-13 19:55:28 -0800

    led array tested and working

--- embedded/src/hardware/led_array.h
+++ embedded/src/hardware/led_array.h
@@ -3,6 +3,7 @@
 
 // Pico SDK
 #include "pico/stdlib.h"
+#include <stdint.h>
 
 // Shared Functions
 void LED_Array_Init(const uint32_t *led_pins, uint32_t pin_number);
---
+++ embedded/src/ui/led_ui.h
@@ -0,0 +1,9 @@
+#ifndef LED_UI_H
+#define LED_UI_H
+#include "../hardware/led_array.h"
+#include "../config.h"
+#include <stdint.h>
+
+void Display_Humidity_LED(float humidity_raw);
+
+#endif
